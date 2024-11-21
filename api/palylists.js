6239e1d8840dd6c5337d788185d0663034c8a228
lucas <lucasdelpriore@gmail.com>
2024-11-21 17:38:54 -0600

    Define models and database, create express app and authentication routes

---
+++ api/palylists.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+module.exports = router;
+
+
+const { authenticate } = require("./auth");
+const prisma = require("../prisma");
+
+
+router.get("/", authenticate, async (req, res, next) => {
+    
+  try {
+    const playlists = await prisma.playlist.findMany({
+      where: { ownerId: req.user.id },
+    });
+    res.json(playlists);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.post("/", authenticate, async (req, res, next) => {
+  const { name, description, trackIds } = req.body;
+
+  try {
+    const tracks = trackIds.map((id) => ({ id }));
+    const playlist = await prisma.playlist.create({
+      data: {
+        name,
+        description,
+        ownerId: req.user.id,
+        tracks: { connect: tracks },
+      },
+    });
+
+    res.status(201).json(playlist);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.get("/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const playlist = await prisma.playlist.findUniqueOrThrow({
+      where: { id: +id },
+      include: { tracks: true },
+    });
+
+    if (playlist.ownerId !== req.user.id) {
+      next({ status: 403, message: "You do not own this playlist." });
+    }
+
+    res.json(playlist);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
